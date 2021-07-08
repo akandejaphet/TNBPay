64e47d4ab89762c2e376d6335b253098596ef65a
Daniel Olowu <79472442+olowudaniel1@users.noreply.github.com>
2021-07-08 02:22:06 +0100

    Update success_modal.php

--- inc/success_modal.php
+++ inc/success_modal.php
@@ -15,24 +15,18 @@ color: #FFFFFF
 }
 
 .await {
-  font-size: 12px;
-  display: flex;
-  flex-direction: row;
-  align-items: stretch;
-    
+  font-size: 14px;
+  color:#ffffff;
 }
 
 .await_payment {
 flex-grow: 3 ;
 
 }
-hr {
-  margin-top:8px;
-  margin-bottom: 10px;
-}
 
 .d-flex {
   text-align: center;
+	padding-top:10px;
 }
 
 .conv_amt {
@@ -50,11 +44,11 @@ font-weight: normal;
 }
 
 .empty_space {
-  height: 8px;
+ 
 }
 
 .empty_space_2 {
-  height: 18px;
+  height: 10px;
 }
 
 .modal-body {
@@ -62,19 +56,22 @@ font-family: Roboto;
 font-style: normal;
 font-weight: normal;
 background: rgba(0, 0, 0, 0.05);
-padding-bottom: 28px ;
-padding-top:  25px;
+padding-top:  12px;
 }
 
 .payment_box {
 background: #FFFFFF;
 display: flex;
 flex-direction: column;
-margin-top: 30px;
-gap: 10px;
-overflow: hidden;
+padding-top:10px;
+height:210px;
+margin-top: 20px;
 }
-
+	
+	.payment_box > div {
+	margin:0 auto;
+	}
+	
 .pay_det {
 font-family: Roboto;
 font-style: normal;
@@ -87,9 +84,13 @@ text-transform: capitalize;
 .pay_box {
   background: rgba(0, 0, 0, 0.05);
   height: 40px;
-  margin: auto;
-  padding: 10px;
+  width:90%;
+   margin: auto;
   text-align: center;
+  font-size:14px;
+  white-space: nowrap;
+  overflow:hidden;
+
 }
 
 .modal-footer {
@@ -114,6 +115,11 @@ border-color: #C80909;
   display: none;
 }
 
+	 @media screen and (min-width: 676px) {
+        .modal-dialog {
+          max-width: 350px; /* New width for default modal */
+        }
+    }
 </style>
 
   <!-- Modal -->
@@ -125,35 +131,31 @@ border-color: #C80909;
         <div class="modal-header">
           <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
           <h4 class="modal-title">TnbPay</h4>
-        </div>
-
-
-        <div class = "await "> 
-          <div class="await_payment"> Awaiting payment </div> 
-          <div class="timer" id="tnb_timer">  </div>
-        </div>
-       <hr/>
-      <br>
+          <div class = "await "> 
+           <div class="timer" id="tnb_timer">  </div>
+        </div> </div>
+    
+     
 
         <div class = "d-flex flex-column"> 
             <div class="p-2 conv_amt"><?php echo( esc_html($price) ); ?> TNB</div> 
-            <div class = "p-2 empty_space">  </div>
+           <!-- <div class = "p-2 empty_space">  </div> -->
             <div class="p-2 conv_rate">1TNB = $<?php echo( esc_html($rate)) ?></div>
-            <div class = "p-2 empty_space_2">  </div>
+             <div class = "p-2 empty_space_2">  </div>
 
          </div>
 
         <div class="modal-body text-center">
-          <p>Send the above amount to the account below using the memo as description</p>
-
+          Send the above amount to the account below using the memo as description
+			
                <div class ="payment_box d-flex flex-column" > 
-                    <div class = "p-2 empty_space"> </div>
+				   
                     <div class = "p-2 pay_det"> Account </div>
                     <div class = "p-2 pay_box "> <?php echo( esc_html($store_address) ); ?></div>
 
                     <div class = "p-2 pay_det"> Memo </div>
                      <div class = "p-2 pay_box"> <?php echo( esc_html($meta) ); ?> </div>
-                     <div class = "p-2 empty_space_2">  </div>
+                     
 
 
                </div>
